4c387b98a0334977ed483b2749f2a4c8cb75df33
PeterKassenaar <info@kassenaar.com>
2024-04-03 13:17:34 +0200

    feat: add signals example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {DeferComponent} from "./components/defer/defer.component";
+import {InjectComponent} from "./components/inject/inject.component";
+import {SignalsComponent} from "./components/signals/signals.component";
+
+interface City {
+  id: number,
+  name: string,
+  country: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DeferComponent, InjectComponent, SignalsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular17';
+  showTitle = false;
+  cities : City[] = [
+    {id: 1, name: 'Amsterdam', country: 'NL'},
+    {id: 2, name: 'Berlin', country: 'GER'},
+    {id: 3, name: 'Tokyo', country: 'JAP'},
+  ]
 }
